cbd63682030d49136094d638294b5633bc0beea3
sappy-joy <stponomarevw@gmail.com>
2020-10-17 16:46:49 +0300

    Изменена структура страниц стихов. Теперь вместо id аргумента в запросе сайт использует страничную структуру

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const slugify = require('slugify');
+
 
 const tempIndex = fs.readFileSync(`${__dirname}/templates/template-index.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
@@ -10,8 +12,13 @@ const dataObj = JSON.parse(data)
 
 const server = http.createServer((req, res) => {
     // console.log(req.url);
-    const { query, pathname } = url.parse(req.url, true);
-    switch (pathname) {
+    const urlName = url.parse(req.url, true);
+    const pathname = urlName.pathname;
+    let page = pathname.substr(
+        0,
+        pathname.indexOf('/', 1) === -1 ? pathname.length : pathname.indexOf('/', 1)
+    );
+    switch (page) {
         case '/':
             res.writeHead(200, {
                 'Content-type': 'text/html'
@@ -26,15 +33,25 @@ const server = http.createServer((req, res) => {
             res.writeHead(200, {
                 'Content-type': 'text/html'
             });
-            const poem = dataObj[query.id];
-            const poemPage = replaceTemplate(tempPoem, poem);
+            page = pathname.substr(pathname.indexOf('/', 1)).substr(1);
+
+            const poem = dataObj.filter(
+                (poem) =>
+                    slugify(slugify(poem.title, {
+                        lower:true,
+                    }), {
+                        remove: /[*+~.()'"!:@]/g
+                    }) === page
+            );
+            if (poem[0] == null) {
+                error404(res);
+                break;
+            }
+            const poemPage = replaceTemplate(tempPoem, poem[0]);
             res.end(poemPage);
             break;
         default:
-            res.writeHead(404, {
-                'Content-type': 'text/html'
-            });
-            res.end('Error 404');
+            error404(res);
     }
 });
 
@@ -42,12 +59,23 @@ server.listen(8000, '127.0.0.1', () => {
     console.log("Listening on the port 8000...");
 });
 
+function error404(res) {
+    res.writeHead(404, {
+        'Content-type': 'text/html'
+    });
+    res.end('Error 404');
+}
+
 function replaceTemplate(temp, poem) {
     preview = getPreview(poem.text);
     let output = temp.replace(/{%TITLE%}/g, poem.title)
         .replace(/{%TEXT%}/g, poem.text)
         .replace(/{%TIME%}/g, poem.time)
-        .replace(/{%ID%}/g, poem.id)
+        .replace(/{%ID%}/g, slugify(slugify(poem.title, {
+            lower:true,
+        }), {
+            remove: /[*+~.()'"!:@]/g
+        }))
         .replace(/{%PREVIEW%}/g, preview);
     return output;
 }
@@ -56,7 +84,7 @@ function getPreview(text) {
     let preview = "";
     let pos = -1;
     let count = 0;
-    while ((pos = text.indexOf('\n', pos + 1)) != -1 && count < 3) {
+    while ((pos = text.indexOf('\n', pos + 1)) !== -1 && count < 3) {
         count++;
     }
     if (pos === -1) {
